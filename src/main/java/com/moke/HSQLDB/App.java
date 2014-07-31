e9084bef88bf98135330bda4c52b8d1e758694fd
mk-win7 <woaishisi@126.com>
2014-07-31 09:53:06 +0800

    初始化hsqldb

---
+++ src/main/java/com/moke/HSQLDB/App.java
@@ -0,0 +1,13 @@
+package com.moke.HSQLDB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
